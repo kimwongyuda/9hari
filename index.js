e5a4c3e7b3b20afe078d29e5ef213baf4dc1ce87
kimwongyuda <rladnjsrb9999@naver.com>
2020-02-06 16:05:51 +0900

    rds

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.get('/api/posts', (req,res)=>{
     var temp = 'select * from posts';
     var ret;
+    console.log(connection);
     connection.query(temp, function(err, rows){
         if(err)
         {
